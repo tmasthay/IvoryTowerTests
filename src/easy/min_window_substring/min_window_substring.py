6d871a1d2f9a09b872dd4a10a4aa4fa126e03147
tmasthay <tyler@ices.utexas.edu>
2024-01-12 12:31:59 -0600

    ADD: min_window_substring

---
+++ src/easy/min_window_substring/min_window_substring.py
@@ -0,0 +1,28 @@
+import hydra
+
+
+def min_window_substring(*args):
+    return 0
+
+
+@hydra.main(config_path=".", config_name="config.yaml", version_base=None)
+def main(cfg):
+    failures = []
+    for test in cfg.tests:
+        res = min_window_substring(*test[0])
+        if type(res) not in [list, set]:
+            success = res == test[1]
+        else:
+            success = set(res) == set([tuple(e) for e in test[1]])
+        if not success:
+            failures.append([test[0], test[1], res])
+    print(f'Passed {len(cfg.tests) - len(failures)} of {len(cfg.tests)}')
+    for failure in failures:
+        print(
+            f'input: {failure[0]}\n   '
+            f' expected_output={failure[1]}\n    actual_output={failure[2]}\n'
+        )
+
+
+if __name__ == "__main__":
+    main()
